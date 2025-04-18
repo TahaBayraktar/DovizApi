6c4631a368cb72420a97b5b5245b2892e1348c20
TahaBayraktar <thbayraktar782@gmail.com>
2025-04-18 18:41:09 +0300

    Delete sarima-api directory

--- sarima-api/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-from model import get_sarima_forecast
-
-app = Flask(__name__)
-
-@app.route("/forecast", methods=["GET"])
-def forecast():
-    result = get_sarima_forecast()
-    return jsonify(result)
